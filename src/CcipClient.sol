2b1b3aeef19076f7a05083026b4031f23292c726
Ihor Bielov <114490078+leyzerok@users.noreply.github.com>
2024-11-16 18:45:10 +0700

    updated remappings file

--- src/CcipClient.sol
+++ src/CcipClient.sol
@@ -1,3 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
+// import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {LinkTokenInterface} from "@chainlink/contracts-ccip/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 contract CcipClient {}
+`
